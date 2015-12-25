0c367ff7f43d4678a606ae1599033cb494d74e80
wanchana <wanchana@perfenterprise.com>
2015-12-25 16:17:26 +0700

    TestMongo

---
+++ ECheckerSource/ApiApp/Models/ReadyStatus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiApp.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ReadyStatus
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// รหัส รถ
+        /// </summary>
+        public int VehicleId { get; set; }
+        /// <summary>
+        /// สถานะความพร้อม
+        /// </summary>
+        public string Status { get; set; }
+        /// <summary>
+        /// ชื่อที่ สร้าง
+        /// </summary>
+        public DateTime CreateDateTime { get; set; }
+    }
+}
