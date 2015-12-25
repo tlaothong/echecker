a1b60e57ef183124e4c8d3e26e1c9229d6eb7a67
Teerachai Laothong <teerachai@perfenterprise.com>
2015-12-25 17:25:18 +0700

    Add DiConfig (Dependency Injection).
    Signed-off-by: Teerachai Laothong <teerachai@perfenterprise.com>

--- ECheckerSource/ApiApp/App_Start/WebApiConfig.cs
+++ ECheckerSource/ApiApp/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using WebApiContrib.IoC.StructureMap;
 
 namespace ApiApp
 {
@@ -17,6 +18,7 @@ namespace ApiApp
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.DependencyResolver = new StructureMapResolver(DiConfig.CreateContainer());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ ECheckerSource/ApiApp/DiConfig.cs
@@ -0,0 +1,19 @@
+﻿using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiApp
+{
+    static class DiConfig
+    {
+        public static IContainer CreateContainer()
+        {
+            return new Container(c =>
+            {
+            });
+        }
+    }
+}
