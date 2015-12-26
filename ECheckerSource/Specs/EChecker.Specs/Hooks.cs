f1126919656fed6f5b30c562a8a0ae5167ba8a98
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2015-12-26 23:40:05 +0700

    Create & setup test project complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ ECheckerSource/Specs/EChecker.Specs/Hooks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace EChecker
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            //TODO: implement logic that has to run before executing each scenario
+        }
+
+        [BeforeScenarioBlock("mock")]
+        public void BeforeScenarioBlock()
+        {
+            if (ScenarioContext.Current.CurrentScenarioBlock == ScenarioBlock.Given)
+            {
+                var mocks = new Moq.MockRepository(Moq.MockBehavior.Strict);
+                ScenarioContext.Current.Set(mocks);
+            }
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            //TODO: implement logic that has to run after executing each scenario
+        }
+    }
+}
