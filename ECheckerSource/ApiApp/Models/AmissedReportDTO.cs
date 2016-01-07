dc227008210e28d0dfcf893a82d5ea778b7d6ff0
sirinarin <sirinarin@perfenterprise.com>
2016-01-07 10:22:58 +0700

    add ReportController.cs

---
+++ ECheckerSource/ApiApp/Models/AmissedReportDTO.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiApp.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AmissedReportDTO
+    {
+        public DateTime CreateDate { get; set; }
+        public IEnumerable<Amissed> Amisseds { get; set; }
+    }
+}
