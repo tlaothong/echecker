a4ae8ecba9dbe43fbd30c5d5039b94a296254e1f
earn <earndarkness@perfenterprise.com>
2016-01-13 21:47:51 +0700

    Clean code on services.ts
    Signed-off-by: earn <earndarkness@perfenterprise.com>

--- ECheckerSource/ECheckerMobile/Scripts/checking/services.ts
+++ ECheckerSource/ECheckerMobile/Scripts/checking/services.ts
@@ -2,19 +2,14 @@
     'use strict';
 
     //Interface form api
-    interface IFormResourceClass<T> extends ng.resource.IResourceClass<ng.resource.IResource<T>> {
-        GetForms(data: T): T;
-    }
+    interface IFormResourceClass<T> extends ng.resource.IResourceClass<ng.resource.IResource<T>> {}
 
     interface ICheckedResourceClass<T> extends ng.resource.IResourceClass<ng.resource.IResource<T>> {
-        GetCheckeds(data: T): T;
         UpdateCheckeds(data: T): T;
     }
    
     //Interface vehicle api
-    interface IStatusResourceClass<T> extends ng.resource.IResourceClass<ng.resource.IResource<T>> {
-        GetReadyStatus(data: T): T;
-    }
+    interface IStatusResourceClass<T> extends ng.resource.IResourceClass<ng.resource.IResource<T>> {}
 
     //Topics service api
     export class FormsService {
@@ -27,38 +22,32 @@
         constructor(appConfig: app.config.IAppConfig, private $resource: angular.resource.IResourceService, private vehicle: app.shared.VehicleService) {
 
             //Set service to get forms
-            this.formSvc = <IFormResourceClass<any>>$resource(appConfig.FormsUrl, { 'id': '@id' }, {
-                GetForms: { method: 'Get', isArray: true }
-            });
+            this.formSvc = <IFormResourceClass<any>>$resource(appConfig.FormsUrl, { 'id': '@id' });
             
             //Set service to get checkes
             this.checkedSvc = <ICheckedResourceClass<any>>$resource(appConfig.CheckedUrl, { 'id': '@VehicleId' }, {
-                GetCheckeds: { method: 'Get' },
                 UpdateCheckeds: { method: 'PUT' }
             });
             
             //Set service to get ready status
-            this.readyStatusSvc = <IStatusResourceClass<any>>$resource(appConfig.ReadyStatusUrl, { 'id': '@id' }, {
-                GetReadyStatus: { method: 'Get' }
-            });
+            this.readyStatusSvc = <IStatusResourceClass<any>>$resource(appConfig.ReadyStatusUrl, { 'id': '@id' });
         }
 
         //Get form datas
-        public GetForms(): ng.IPromise<TopicInformation[]> {
+        public GetForms(): ng.IPromise<any> {
             var formId = this.vehicle.VehicleSelected.VehicleTypeId;
             var IsFormIdMatch = ((formId == 11) || (formId == 13)) ? true : false;
             if (!IsFormIdMatch) {
                 //Hack: mock form id
                 formId = 11;
             }
-            return this.formSvc.GetForms(new FormRequest(formId)).$promise;
+            return this.formSvc.query({ id: formId }).$promise;
         }
         
         //Get checked datas
         public GetCheckeds(): ng.IPromise<CheckedInformation> {
             var vehicleId = this.vehicle.VehicleSelected.id;
-            var result = this.checkedSvc.get({ id: vehicleId }).$promise;
-            return result;
+            return this.checkedSvc.get({ id: vehicleId }).$promise;
         }
 
         public UpdateCheckeds(checkeds: CheckedInformation): void {
@@ -67,7 +56,7 @@
 
         public GetReadyStatus(): ng.IPromise<Object> {
             var vehicleId = this.vehicle.VehicleSelected.id
-            return this.readyStatusSvc.GetReadyStatus(new GetReadyStatusRequest(vehicleId)).$promise;
+            return this.readyStatusSvc.get({ id: vehicleId }).$promise;
         }
     }
 
