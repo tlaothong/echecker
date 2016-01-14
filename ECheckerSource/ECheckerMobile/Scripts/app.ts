c8bea804a70972f42dc58bfb9544088a79dbf263
Bank <a@a.com>
2016-01-14 09:32:05 +0700

    modal login

--- ECheckerSource/ECheckerMobile/Scripts/app.ts
+++ ECheckerSource/ECheckerMobile/Scripts/app.ts
@@ -30,7 +30,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'app', 'app.shared', 
                 abstract: true,
 
                 templateUrl: 'templates/sidemenu.html',
-                controller: 'AppCtrl'
+                controller: 'AppCtrl as cx'
             })
 
             .state('app.vehicles', {
